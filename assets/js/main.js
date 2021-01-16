43a35c3e5f50b2e80cf9981825c5f0315a519404
bartvtw <12793257+bartvantilburgwagemakers@users.noreply.github.com>
2021-01-16 11:16:29 +0100

    Trigger question div

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 var Coordinates = [];
 var TravelDistance = 0;
+var LastTravelDistance = 0;
 var LastLocation;
+var QuestionsDone = [];
 
 function interval() {
   var div = document.getElementById("debug-Info");
@@ -11,8 +13,9 @@ function interval() {
     addDiv("loc  " + currentLoc, div);
     if (LastLocation) {
       var distance = getDistance(LastLocation, currentLoc);
+      addDiv("distance is   " + distance, div);
       console.log("distance is " + distance);
-      if (distance > 5) {
+      if (distance > 25) {
         LastLocation = currentLoc;
         TravelDistance = TravelDistance + distance;
         Coordinates.push(currentLoc);
@@ -22,14 +25,26 @@ function interval() {
       LastLocation = currentLoc;
     }
   });
-}
+  if(LastTravelDistance != TravelDistance){
+    var diff = TravelDistance - LastTravelDistance;
+    console.log("diff in travalDist "+diff);
+    if(diff>200){
+      NextQuestion();
+      LastTravelDistance = TravelDistance;
+    }
+  }else if(LastTravelDistance == 0){
+    LastTravelDistance = TravelDistance;
+  }
+  }
 
 function getDistance(firstCoordinate, lastCoordinate) {
   var phi1 = (firstCoordinate[0] * Math.PI) / 180;
   var phi2 = (lastCoordinate[0] * Math.PI) / 180;
   var dLambda = ((firstCoordinate[1] - lastCoordinate[1]) * Math.PI) / 180;
 
-  with (Spheroid.wgs84) var s = distance(phi1, phi2, dLambda);
+  with (Spheroid.wgs84) {
+    var s = distance(phi1, phi2, dLambda);
+  }
 
   return s;
 }
@@ -40,4 +55,25 @@ function addDiv(innerHTML, element) {
   var newElement = document.createElement("div");
   newElement.innerHTML = innerHTML;
   element.appendChild(newElement);
+}
+
+function Answer() {
+  var answerBtn = document.getElementById("answer");
+  if (answerBtn) {
+    var answerBtn = answerBtn.value;
+    console.log(answerBtn);
+    if ("{{page.answerBtn}}" === answerBtn) {
+      alert("goed zo");
+    } else {
+      alert(answerBtn + " is fout");
+    }
+  }
+}
+
+function NextQuestion(){
+  console.log("NextQuestion");
+  var questionDiv = document.getElementById("question");
+  questionDiv.style.visibility = "visible";
+  document.getElementById("questionTest").innerHTML="dit is een vraag";
+
 }
--- pages/main.md
+++ pages/main.md
@@ -9,6 +9,8 @@ description: Om de x aantal meter kunt u een nieuwe vraag krijgen.
 We zulle eerst even wat moet lopen
 
 <div id="question" style ="visibility: hidden;" >
+    <p id=questionTest></p>
+    <div id="media"></div>
     <input type="text" id="answer" name="answer"/>
     <button id="answerBtn">Click me</button>
 </div>
