4ffc9c7801ad46ae1dcde9a691a8165eda6966a4
bart van tilburg-wagemakers <12793257+bartvantilburgwagemakers@users.noreply.github.com>
2021-01-16 09:30:05 +0100

    adds debug info

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-var coordinates = [];
+var Coordinates = [];
+var TravelDistance = 0;
+
+function interval(){
+    var div = document.getElementById("debug-Info");
+    addDiv("travel distance "+TravelDistance ,div);
+    TravelDistance++;
+}
+
+// window.setInterval(interval, 3000);
+window.setInterval(interval, 30000);
+
+  function addDiv(innerHTML, element){
+    var newElement = document.createElement("div");
+    newElement.innerHTML = innerHTML;
+    element.appendChild(newElement);
+  }
